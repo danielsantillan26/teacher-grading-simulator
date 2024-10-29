e74e2f2046262f1eedd5831022bf452afcbfd495
Daniel Santillan <daniel.santillan26@bishopireton.org>
2024-10-29 12:48:34 -0400

    Attempt JTable on Add Assignments card

--- TeacherGradingSystem/src/graphics/AddAssignments.java
+++ TeacherGradingSystem/src/graphics/AddAssignments.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 public class AddAssignments extends JPanel {
 	
@@ -14,7 +15,11 @@ public class AddAssignments extends JPanel {
 	
 	private FlowLayout fl;
 	private BorderLayout bl;
-	private JPanel flPanel;
+	private JPanel flPanel, centerPanel;
+	
+	private JTable table;
+	
+	
 	
 	
 	public AddAssignments() {
@@ -27,6 +32,24 @@ public class AddAssignments extends JPanel {
 		flPanel.setBackground(GraphicsConstants.headerColor);
 		flPanel.setLayout(fl);	
 		add(flPanel, BorderLayout.NORTH);
+		
+		centerPanel = new JPanel();
+		centerPanel.setBackground(GraphicsConstants.backgroundColor);
+		
+		String[] columnTest = {"Assignments", "Grading System", "Weight", "Point"
+				+ " Value"};
+		Object[][] values = {{"Test", "Test", "Test", "Test", "Test"}, {
+				"Test", "Test", "Test", "Test", "Test"}, {"Test", "Test", 
+				"Test", "Test", "Test"}};
+		table = new JTable(values, columnTest);
+		table.getTableHeader().setBounds(100, 100, getWidth() - 100, 175);
+		table.setBounds(100, 175, getWidth() - 100, getHeight() - 300);
+		centerPanel.add(table.getTableHeader());
+		centerPanel.add(table);
+		add(centerPanel, BorderLayout.CENTER);
+		
+		
+		
 	}
 	
 	
